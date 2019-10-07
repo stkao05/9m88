e989c23a3ed6f9ccb783aaf49b2e9ba0f93990bc
stkao05 <st.kao.05@gmail.com>
2019-10-07 22:16:46 +0800

    - tour

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -4,13 +4,14 @@ export default function Tour() {
   return (
     <div className="tour light_background">
       <h1>Tour</h1>
+
       <table>
         <thead>
           <tr>
             <th>Time</th>
             <th>Location</th>
             <th>Venue</th>
-            <th>Event link</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -18,7 +19,25 @@ export default function Tour() {
             <td>19 Dec 19, 20 pm</td>
             <td>Philadelphia, PA, United States</td>
             <td>The Fox Theater Pomona</td>
-            <td>Get Ticket</td>
+            <td className="ticket">
+              <a className="btn btn_invert" href="">Ticket</a>
+            </td>
+          </tr>
+          <tr>
+            <td>19 Dec 19, 20 pm</td>
+            <td>Philadelphia, PA, United States</td>
+            <td>The Fox Theater Pomona</td>
+            <td className="ticket">
+              <a className="btn btn_invert" href="">Ticket</a>
+            </td>
+          </tr>
+          <tr>
+            <td>19 Dec 19, 20 pm</td>
+            <td>Philadelphia, PA, United States</td>
+            <td>The Fox Theater Pomona</td>
+            <td className="ticket">
+              <a className="btn btn_invert" href="">Ticket</a>
+            </td>
           </tr>
         </tbody>
       </table>
