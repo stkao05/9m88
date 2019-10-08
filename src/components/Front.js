d77b70d15f6dfcb40376afb701eda9c94f61fc67
stkao05 <st.kao.05@gmail.com>
2019-10-08 15:18:47 +0800

    front

--- src/components/Front.js
+++ src/components/Front.js
@@ -4,6 +4,12 @@ import Logo from "./Logo"
 export default function Front() {
   return (
     <div className="front">
+      <div className="cover">
+        <span className="title en">Beyond Mediocrity</span>
+        <span className="title zh">平庸之上</span>
+
+        <img src="./images/cover.png" alt="" />
+      </div>
 
       <img
         className="background"
