dcaa0acfc9eebe9f1a7252cc09a848b003f0140a
stkao05 <st.kao.05@gmail.com>
2019-10-10 21:35:31 +0800

    youtube

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,24 +3,24 @@ import "./background_video.js"
 import { Youtube } from "./VideoSources"
 
 export default function Video() {
+  //return (
+    //<div className="video parallex">
+      //<div className="blocking_overlay" />
+
+      //<div className="parallex_background">
+        //<video className="direct_video" autoplay="1" loop muted playsinline="1">
+          //<source src="./aim_high.mp4" type="video/mp4" />
+        //</video>
+      //</div>
+    //</div>
+  //)
   return (
     <div className="video parallex">
       <div className="blocking_overlay" />
 
       <div className="parallex_background">
-        <video className="direct_video" autoplay="1" loop muted playsinline="1">
-          <source src="./aim_high.mp4" type="video/mp4" />
-        </video>
+        <Youtube />
       </div>
     </div>
   )
-  //return (
-  //<div className="video parallex">
-  //<div className="blocking_overlay" />
-
-  //<div className="parallex_background">
-  //<Youtube />
-  //</div>
-  //</div>
-  //)
 }
