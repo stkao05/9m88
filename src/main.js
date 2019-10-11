c46b7d361d6f409bf0058c731e1543bc4f4d072f
stkao05 <st.kao.05@gmail.com>
2019-10-11 13:10:16 +0800

    build

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 ;(function() {
   function setupYoutube() {
-    // loads the IFrame Player API code asynchronously.
-    var tag = document.createElement("script")
-    tag.src = "https://www.youtube.com/player_api"
-
-    var firstScriptTag = document.getElementsByTagName("script")[0]
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
-
     var playerElementId = "youtube_player"
     var player
 
